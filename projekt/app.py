f1a3a1abaa0fbc4d82c8a96542ae7ce9da1817b5
Kamzie1 <kamilzie07@gmail.com>
2025-05-28 22:34:45 +0200

    set caption dla okienka

--- projekt/app.py
+++ projekt/app.py
@@ -1,29 +1,31 @@
 import pygame
 
+
 def main():
-  """
+    """
     główna pętla pygame
-  """
+    """
 
-  pygame.init() #inicjalizacja pygame
+    pygame.init()  # inicjalizacja pygame
 
-  Width, Height = 600, 600
-  screen = pygame.display.set_mode((Width, Height)) #okienko
-  clock = pygame.time.Clock() #czas
-  FPS = 60
+    Width, Height = 600, 600
+    screen = pygame.display.set_mode((Width, Height))  # okienko
+    pygame.display.set_caption("Gra")
+    clock = pygame.time.Clock()  # czaspy
+    FPS = 60
 
-  dziala = True
-  while dziala:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT: #jeżeli nacisnąłeś wyjście z okienka to wychodzisz z pętli, pygame dziala tak ze input użytkownika nie stopuje programu 
-        dziala = False
+    dziala = True
+    while dziala:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # wyjdź z programu
+                dziala = False
 
-    screen.fill('blue') #wypaełnia screena na niebiesko
-    pygame.display.flip() #odświeża display
+        screen.fill("blue")  # wypaełnia screena na niebiesko
+        pygame.display.flip()  # odświeża display
 
-    clock.tick(FPS) # maks 60 FPS, szybkosc petli bedzie mniej wiecej taka sama dla wszystkich użytkowników nawet tych z wypasionym kompem
+        clock.tick(FPS)  # maks 60 FPS
 
-  
+
 # ważne!!! Odpala tylko, jeżeli został uruchomiony sam z siebie, a nie w formie zainportowanego modułu. Bez tego, gdybyśmy importwali ten program to przy imporcie uruchamiałby się main()
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
